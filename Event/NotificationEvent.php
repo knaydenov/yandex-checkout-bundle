b4c4d066f497898258f8b929fa1857ab7cfdb279
Klim Naydenov <knaydenov@gmail.com>
2018-12-09 21:27:17 +0300

    Add event handling

---
+++ Event/NotificationEvent.php
@@ -0,0 +1,50 @@
+<?php
+namespace Kna\YandexCheckoutBundle\Event;
+
+
+use Symfony\Component\EventDispatcher\Event;
+use YandexCheckout\Model\Notification\AbstractNotification;
+
+class NotificationEvent extends Event
+{
+    /**
+     * @var AbstractNotification
+     */
+    protected $notification;
+
+    /**
+     * @var bool
+     */
+    protected $accepted = false;
+    /**
+     * @return AbstractNotification
+     */
+    public function getNotification(): AbstractNotification
+    {
+        return $this->notification;
+    }
+
+    /**
+     * @param AbstractNotification $notification
+     */
+    public function setNotification(AbstractNotification $notification): void
+    {
+        $this->notification = $notification;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAccepted(): bool
+    {
+        return $this->accepted;
+    }
+
+    /**
+     * @param bool $accepted
+     */
+    public function setAccepted(bool $accepted): void
+    {
+        $this->accepted = $accepted;
+    }
+}
---
+++ Events.php
@@ -0,0 +1,8 @@
+<?php
+namespace Kna\YandexCheckoutBundle;
+
+
+class Events
+{
+    const NOTIFICATION_RECEIVED = 'kna_yandex_checkout.notification_received';
+}
