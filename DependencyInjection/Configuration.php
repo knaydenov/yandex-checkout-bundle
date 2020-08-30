8f12e38179aa063ecd6970a9bc63d29f4e909806
Klim Naydenov <knaydenov@gmail.com>
2020-08-30 18:54:57 +0300

    Add SF5 support

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -13,8 +13,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $root = $treeBuilder->root('kna_yandex_checkout');
+        $treeBuilder = new TreeBuilder('kna_yandex_checkout');
+        $root = $treeBuilder->getRootNode();
 
         $root
             ->children()
